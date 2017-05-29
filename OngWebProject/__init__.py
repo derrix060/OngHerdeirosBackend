9e197ee289dd300e911b12d8ee9484d2f03d12d2
Mario Apra <mario_surfing@hotmail.com>
2017-05-29 17:57:58 -0300

    change main file

--- OngWebProject/__init__.py
+++ OngWebProject/__init__.py
@@ -1,16 +1,1 @@
-from flask import Flask
 
-app = Flask(__name__)
-events_path = './OngWebProject/sources/events.json'
-itens_path = './OngWebProject/sources/itens.json'
-
-f = open(events_path, 'r')
-events = []
-events = f.read()
-f.close()
-f = open(itens_path, 'r')
-itens = []
-itens = f.read()
-f.close()
-
-import OngWebProject.routes
--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from OngWebProject import app
+from flask import Flask
+
+app = Flask(__name__)
+events_path = './OngWebProject/sources/events.json'
+itens_path = './OngWebProject/sources/itens.json'
+
+f = open(events_path, 'r')
+events = []
+events = f.read()
+f.close()
+f = open(itens_path, 'r')
+itens = []
+itens = f.read()
+f.close()
+
+import OngWebProject.routes
 
 if __name__ == '__main__':
     app.run(threaded=True)
