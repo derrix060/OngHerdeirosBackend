dba1e5f2a9e907756e8f4fac323740993680d2b9
Mario Apra <mario_surfing@hotmail.com>
2017-05-29 18:22:08 -0300

    test make every route on main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 events_path = './OngWebProject/sources/events.json'
@@ -13,17 +13,59 @@ itens = []
 itens = f.read()
 f.close()
 
-import OngWebProject.routes
+@app.route('/')
+def hello_world():
+    return 'Hello, World! - v1'
 
+@app.route('/1')
+def hello_1():
+    return 'Hello, 1'
+
+@app.route('/api/herdeiros/eventos/all')
+def get_all_events():
+    return events
+
+
+@app.route('/api/herdeiros/itens/all')
+def get_all_itens():
+    return jsonify(itens)
+
+
+@app.route('/create_events')
+def create_events():
+    eventos = []
+    evento = {}
+    evento['name'] = 'teste name'
+    evento['date'] = "2017-06-05 17:00:00"
+    eventos.append(evento)
+    eventos.append(evento)
+    eventos.append(evento)
+    rtn = jsonify(eventos)
+    f = open(events_path, 'w+')
+    f.write(rtn)
+    f.close()
+    return rtn
+
+
+@app.route('/api/herdeiros/eventos/add/<name>/<date>')
+def add_event(name, date):
+    event = Event(name, date)
+    events.append(event)
+    f = open(events_path, 'w+')
+    f.write(events)
+    f.close()
+    return 'Event added!'
 
 @app.route('/2')
 def hello_world2():
-    return 'Hello, World! - v2'
+    return '/2'
 
 
 @app.route('/3')
 def hello_world3():
-    return 'Hello, World! - v3'
+    return '/3'
 
-if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+app.run(threaded=True)
+app.config.update(
+  DEBUG=True,
+  PROPAGATE_EXCEPTIONS=True)
